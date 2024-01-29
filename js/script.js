541f2401ca4e870720f9e6638223decfeaad712c
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-01-29 13:43:56 +0100

    added 10 rendom email

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 console.log('Vue ok', Vue);
 
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
+
 const {createApp} = Vue;
 const app = createApp({
     name: 'Vue Slider',
     data: () => ({
-        email: null
+        emails: []
       
-    })
+    }),
+    created(){
+        for(let i=0; i < 10; i++){
+
+            axios.get(endpoint).then((res) => {
+                const mail = res.data.response;
+                this.emails.push(mail)
+            })
+        }
+    }
 })
 
 app.mount('#root')
